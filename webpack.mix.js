6ced230c7ca269f7350f745170a85722aa7075fe
salome batkhadze <salome.batkhadze@gmail.com>
2021-07-19 21:59:41 +0400

    add index

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,5 +3,5 @@
 let mix = require('laravel-mix');
 
 
-mix.js('src/js/app.js', 'dist')
-    .sass('src/scss/app.scss', 'dist')
+mix.js('src/js/app.js', 'public/dist')
+    .sass('src/scss/app.scss', 'public/dist')
